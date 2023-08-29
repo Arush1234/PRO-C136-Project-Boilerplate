552f34fb8ae4cca4ab97795ae26817332c5b3b32
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2023-08-29 19:06:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# api listening to POST requests and predicting sentiments
 @app.route('/predict' , methods = ['POST'])
 def predict():
 
@@ -18,8 +17,6 @@ def predict():
                     'message' : 'Empty Review'}
     
     else:
-
-        # calling the predict method from prediction.py module
         sentiment , path = prediction.predict(review)
         response = {'status' : 'success',
                     'message' : 'Got it',
@@ -30,26 +27,26 @@ def predict():
 
 
 # Creating an API to save the review, user clicks on the Save button
-@app.route('/' , methods = [''])
+@app.route('/save' , methods = ['POST'])
 def save():
-
     # extracting date , product name , review , sentiment associated from the JSOn data
-    date = request.json.get('')
-    product = request.json.get('')
-    review = request.json.get('')
-    sentiment = request.json.get('')
+    date = request.json.get('date')
+    product = request.json.get('product')
+    review = request.json.get('review')
+    sentiment = request.json.get('sentiment')
 
     # creating a final variable seperated by commas
     data_entry = date + "," + product + "," + review + "," + sentiment
 
     # open the file in the 'append' mode
+    f = open('./static/assets/datafiles/data_entry.csv' , 'a')
 
     # Log the data in the file
+    f.write(data_entry + '\n')
 
     # return a success message
     return jsonify({'status' : 'success' , 
                     'message' : 'Data Logged'})
-
 
 if __name__  ==  "__main__":
     app.run(debug = True)
